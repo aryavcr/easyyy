7f94204e7e58a30fc77b1c49629e66f6870314bd
aryavcr <174735438+aryavcr@users.noreply.github.com>
2025-04-22 02:00:08 +0530

    minor change

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import Profile from "@/components/profile";
-import { Settings2, UserPen } from "lucide-react";
+import { UserPen } from "lucide-react";
 
 const Settings = () => {
   return (
     <div className=" ml-10 pl-6 h-[97%] w-[94%]">
-      <div className="flex items-center gap-2 p-3 font-bold tracking-tight sticky text-3xl">
-        <Settings2 />
-        Settings
-      </div>
       <div className="w-full h-[90%] p-15 justify-center">
         <div className="w-[40%] flex gap-2 text-lg font-medium mb-10">
           <UserPen />
